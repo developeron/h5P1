aad10a6050b39c8f7e0b2afced74700dbace5784
onaseer07 <onaseer07@gmail.com>
2018-07-10 00:24:03 -0500

    Updated Layout

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,16 +1,13 @@
 $(document).ready(function(){
     $(window).scroll(function() {
-        if ($(document).scrollTop() > 500) {
-            $('#siteNav').css({
-                'background-color': '#efcc31',
-                'box-shadow': 'box-shadow: 2px 0px 10px #0000003e'
-            });
+        if ($(document).scrollTop() < 50) {
+            $('#siteNav').css('background-color', '#efcc31;');
         } else {
             $('#siteNav').css('background-color', 'transparent');
         }
-    })
+    });
 
-    $.backstretch('assets/images/events/basant.png')
+    $.backstretch('assets/images/events/basant.jpg');
     /*$('.bg').backstretch([
         "assets/images/events/basant.png"
       , "assets/images/entrees/beefOverRice.jpg"
@@ -18,12 +15,34 @@ $(document).ready(function(){
     ], {duration: 3000, fade: 750});*/
 
     let controller = new ScrollMagic.Controller();
+    
+   /* let nav = new ScrollMagic.Scene({
+        triggerElement: '#aboutUs',
+        triggerHook: 1,
+        reverse: true,
+    })
+        .setClassToggle('#siteNav','fade-in')
+        .addTo(controller);*/
     let logo = new ScrollMagic.Scene({
         triggerElement: '#intro h1',
         triggerHook: 1,
         reverse: false,
     })
         .setClassToggle('#intro','fade-in')
+        .addTo(controller);
+    let aboutUs = new ScrollMagic.Scene({
+        triggerElement: '#aboutUs',
+        triggerHook: 1,
+        reverse: false,
+    })
+        .setClassToggle('#aboutUs','fade-in')
+        .addTo(controller);
+    let menu = new ScrollMagic.Scene({
+        triggerElement: '#menuContent',
+        triggerHook: 1,
+        reverse: false,
+    })
+        .setClassToggle('#menu','fade-in')
         .addTo(controller);
 
 });
