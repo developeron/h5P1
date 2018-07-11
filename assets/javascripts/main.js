0bcdf70277758865850df2bd943078604cb33650
onaseer07 <onaseer07@gmail.com>
2018-07-11 11:41:03 -0500

    updated bg

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,28 +1,15 @@
 $(document).ready(function(){
-    $(window).scroll(function() {
-        if ($(document).scrollTop() < 50) {
-            $('#siteNav').css('background-color', '#efcc31;');
-        } else {
-            $('#siteNav').css('background-color', 'transparent');
-        }
-    });
-
+//navbar on scroll
+$(window).scroll(function(){
+    if ($(document).scrollTop() > 50){
+        $('#slogan').css('opacity','0')
+    };
+})
+    //backstretch.js
     $.backstretch('assets/images/events/basant.jpg');
-    /*$('.bg').backstretch([
-        "assets/images/events/basant.png"
-      , "assets/images/entrees/beefOverRice.jpg"
-      , "assets/images/events/basant.jpg"
-    ], {duration: 3000, fade: 750});*/
-
+    //scrollMagic.js
     let controller = new ScrollMagic.Controller();
     
-   /* let nav = new ScrollMagic.Scene({
-        triggerElement: '#aboutUs',
-        triggerHook: 1,
-        reverse: true,
-    })
-        .setClassToggle('#siteNav','fade-in')
-        .addTo(controller);*/
     let logo = new ScrollMagic.Scene({
         triggerElement: '#intro h1',
         triggerHook: 1,
